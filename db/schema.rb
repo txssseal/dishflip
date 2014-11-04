5fa130f6413b4c5b9b6450155422c97c069d069f
Colton <colton@coltonseal.com>
2014-11-04 13:41:56 -0600

    phone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104191808) do
+ActiveRecord::Schema.define(version: 20141104194055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20141104191808) do
     t.string   "email"
     t.string   "website"
     t.string   "facebook"
+    t.string   "phone"
   end
 
   create_table "rests", force: true do |t|
